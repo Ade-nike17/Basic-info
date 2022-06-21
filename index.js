754ed74ee9c4bab0caef1af291fec4ab8bea10fa
Nyke <perfectnyke@gmail.com>
2022-06-21 15:05:04 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+let name = prompt('Enter your name');
+let height = prompt('Enter your height');
+let country = prompt('Enter your country');
+
+document.write(name + '<br>' + height + '<br>' + country);
